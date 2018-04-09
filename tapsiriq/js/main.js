8ed77d1ccef2f72b9a8faf4da50f0b66ad94b2a9
nahid1905 <36768178+nahid1905@users.noreply.github.com>
2018-04-09 21:35:22 +0400

    Add files via upload

---
+++ tapsiriq/js/main.js
@@ -0,0 +1,109 @@
+var t1 = document.querySelector("#t1");
+var t2 = document.querySelector("#t2");
+var t3 = document.querySelector("#t3");
+var t4 = document.querySelector("#t4");
+var t5 = document.querySelector("#t5");
+var t6 = document.querySelector("#t6");
+var t7 = document.querySelector("#t7");
+var t8 = document.querySelector("#t8");
+var text1 = document.querySelector(".text1")
+var text2 = document.querySelector(".text2")
+var text3 = document.querySelector(".text3")
+var text4 = document.querySelector(".text4")
+var text5 = document.querySelector(".text5")
+var text6 = document.querySelector(".text6")
+var text7 = document.querySelector(".text7")
+var text8 = document.querySelector(".text8")
+var col = document.querySelector(".col-md-8")
+
+
+t1.addEventListener("click",function() {
+	text1.style.display="block";
+	text2.style.display="none";
+	text3.style.display="none";
+	text4.style.display="none";
+	text5.style.display="none";
+	text6.style.display="none";
+	text7.style.display="none";
+	text8.style.display="none";
+
+
+})
+t2.addEventListener("click",function() {
+	text1.style.display="none";
+	text3.style.display="none";
+	text4.style.display="none";
+	text5.style.display="none";
+	text6.style.display="none";
+	text7.style.display="none";
+	text8.style.display="none";
+	text2.style.display="block";
+
+})
+t3.addEventListener("click",function() {
+	text3.style.display="block";
+	text1.style.display="none";
+	text2.style.display="none";
+	text4.style.display="none";
+	text5.style.display="none";
+	text6.style.display="none";
+	text7.style.display="none";
+	text8.style.display="none";
+
+})
+t4.addEventListener("click",function() {
+	text4.style.display="block";
+	text1.style.display="none";
+	text2.style.display="none";
+	text3.style.display="none";
+	text5.style.display="none";
+	text6.style.display="none";
+	text7.style.display="none";
+	text8.style.display="none";
+
+})
+t5.addEventListener("click",function() {
+	text5.style.display="block";
+	text1.style.display="none";
+	text2.style.display="none";
+	text3.style.display="none";
+	text4.style.display="none";
+	text6.style.display="none";
+	text7.style.display="none";
+	text8.style.display="none";
+
+})
+t6.addEventListener("click",function() {
+	text6.style.display="block";
+	text1.style.display="none";
+	text2.style.display="none";
+	text3.style.display="none";
+	text4.style.display="none";
+	text5.style.display="none";
+	text7.style.display="none";
+	text8.style.display="none";
+
+})
+t7.addEventListener("click",function() {
+	text7.style.display="block";
+	text1.style.display="none";
+	text2.style.display="none";
+	text3.style.display="none";
+	text4.style.display="none";
+	text5.style.display="none";
+	text6.style.display="none";
+	text8.style.display="none";
+
+})
+t8.addEventListener("click",function() {
+	text8.style.display="block";
+	text1.style.display="none";
+	text2.style.display="none";
+	text3.style.display="none";
+	text4.style.display="none";
+	text5.style.display="none";
+	text6.style.display="none";
+	text7.style.display="none";
+
+})
+
